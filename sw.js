a6b790eb3b4d478a55486d374b50cbd8c42d6029
patrick <patrick204nqh@gmail.com>
2020-06-08 12:39:55 +0700

    pre-caching assets

--- sw.js
+++ sw.js
@@ -1,6 +1,24 @@
+const staticCacheName = 'site-static';
+const assets = [
+  '/',
+  '/index.html',
+  '/js/app.js',
+  '/js/ui.js',
+  '/css/styles.css',
+  '/css/materialize.min.css',
+  '/img/dish.png',
+  'https://fonts.gooleapis.com/icon?family=Material+Icons',
+];
+
 // install service worker
 self.addEventListener('install', (evt) => {
-  console.log('service worker has been installed');
+  // console.log('service worker has been installed');
+  evt.waitUntil(
+    caches.open(staticCacheName).then((cache) => {
+      console.log('caching shell assets');
+      cache.addAll(assets);
+    })
+  );
 });
 
 // activate event
@@ -10,5 +28,5 @@ self.addEventListener('activate', (evt) => {
 
 // fetch event
 self.addEventListener('fetch', (evt) => {
-  console.log('fetch event', evt);
+  // console.log('fetch event', evt);
 });
