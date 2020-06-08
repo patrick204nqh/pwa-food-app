f93a61f581d7fe0641932238e933d6d31d797d40
patrick <patrick204nqh@gmail.com>
2020-06-08 14:35:40 +0700

    dynamic caching

--- sw.js
+++ sw.js
@@ -1,4 +1,5 @@
-const staticCacheName = 'site-static';
+const staticCacheName = 'site-static-v1';
+const dynamicCacheName = 'site-dynamic-v1';
 const assets = [
   '/',
   '/index.html',
@@ -43,7 +44,15 @@ self.addEventListener('fetch', (evt) => {
   // console.log('fetch event', evt);
   evt.respondWith(
     caches.match(evt.request).then((cacheRes) => {
-      return cacheRes || fetch(evt.request);
+      return (
+        cacheRes ||
+        fetch(evt.request).then((fetchRes) => {
+          return caches.open(dynamicCacheName).then((cache) => {
+            cache.put(evt.request.url, fetchRes.clone());
+            return fetchRes;
+          });
+        })
+      );
     })
   );
 });
