c2c6e70b8f594483d4184b68bc5682e7fca6b318
patrick <patrick204nqh@gmail.com>
2020-06-09 14:50:57 +0700

    adding recipes

--- js/db.js
+++ js/db.js
@@ -23,3 +23,22 @@ db.collection('recipes').onSnapshot((snapshot) => {
     }
   });
 });
+
+// add new recipe
+const form = document.querySelector('form');
+
+form.addEventListener('submit', (evt) => {
+  evt.preventDefault();
+
+  const recipe = {
+    title: form.title.value,
+    ingredients: form.ingredients.value,
+  };
+
+  db.collection('recipes')
+    .add(recipe)
+    .catch((err) => console.log(err));
+
+  form.title.value = '';
+  form.ingredients.value = '';
+});
